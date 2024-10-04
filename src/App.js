e55f1f072854423fd311144d6d845fef0c0754be
Bao Nguyen <g00420095@atu.ie>
2024-10-04 10:46:03 +0000

    Part 3 completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Content from './components/content';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   let x = 5;
@@ -9,6 +10,8 @@ function App() {
         asdf{}
         <h2>It is {new Date().toLocaleDateString()}</h2>
         <Content></Content>
+        <Header></Header>
+        <Footer></Footer>
     </div>
   );
 }
